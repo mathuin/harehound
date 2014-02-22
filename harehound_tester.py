0e48a46760aecb17491c206b4a4874641f9b44cd
Jack Twilley <mathuin@gmail.com>
2014-02-22 14:42:42 -0800

    Modified test code to run on localhost or the target server.
    Removed hardcoded host information.  Will add support for changing
    ports later if necessary.

--- harehound_tester.py
+++ harehound_tester.py
@@ -5,12 +5,18 @@ Testing the hounds and hare server!
 """
 
 import socket
+import sys
 
-def test_server(datain, dataout):
+if len(sys.argv) == 2:
+    print 'host is %s' % sys.argv[1]
+    host = sys.argv[1]
+else:
+    host = ''
+port = 50000
+size = 1024
+
+def test_server(datain, dataout, host=host, port=port, size=size):
     print "Sending '%s', expecting '%s'" % (datain, dataout)
-    host = 'twilley.org'
-    port = 50000
-    size = 1024
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host,port))
     s.send(datain)
